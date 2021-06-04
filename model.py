0255fcc421c89966b693a0de2e11d6895ea033a0
imcosmicowl <marcos.melgar.segovia@gmail.com>
2021-06-04 10:06:04 +0200

    prettified text format

--- model.py
+++ model.py
@@ -1,20 +1,22 @@
 import torch
 import torch.nn as nn
 
+
 class ParallelModel(nn.Module):
     """"
     parallel cnn transformer
     """
-    def __init__(self,num_emotions):
+
+    def __init__(self, num_emotions):
         super().__init__()
         # conv block
         self.conv2Dblock = nn.Sequential(
             # 1. conv block
             nn.Conv2d(in_channels=1,
-                       out_channels=16,
-                       kernel_size=3,
-                       stride=1,
-                       padding=1
+                      out_channels=16,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1
                       ),
             nn.BatchNorm2d(16),
             nn.ReLU(),
@@ -22,10 +24,10 @@ class ParallelModel(nn.Module):
             nn.Dropout(p=0.3),
             # 2. conv block
             nn.Conv2d(in_channels=16,
-                       out_channels=32,
-                       kernel_size=3,
-                       stride=1,
-                       padding=1
+                      out_channels=32,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1
                       ),
             nn.BatchNorm2d(32),
             nn.ReLU(),
@@ -33,10 +35,10 @@ class ParallelModel(nn.Module):
             nn.Dropout(p=0.3),
             # 3. conv block
             nn.Conv2d(in_channels=32,
-                       out_channels=64,
-                       kernel_size=3,
-                       stride=1,
-                       padding=1
+                      out_channels=64,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1
                       ),
             nn.BatchNorm2d(64),
             nn.ReLU(),
@@ -44,10 +46,10 @@ class ParallelModel(nn.Module):
             nn.Dropout(p=0.3),
             # 4. conv block
             nn.Conv2d(in_channels=64,
-                       out_channels=64,
-                       kernel_size=3,
-                       stride=1,
-                       padding=1
+                      out_channels=64,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1
                       ),
             nn.BatchNorm2d(64),
             nn.ReLU(),
@@ -55,21 +57,23 @@ class ParallelModel(nn.Module):
             nn.Dropout(p=0.3)
         )
         # Transformer block
-        self.transf_maxpool = nn.MaxPool2d(kernel_size=[2,4], stride=[2,4])
-        transf_layer = nn.TransformerEncoderLayer(d_model=64, nhead=4, dim_feedforward=512, dropout=0.4, activation='relu')
+        self.transf_maxpool = nn.MaxPool2d(kernel_size=[2, 4], stride=[2, 4])
+        transf_layer = nn.TransformerEncoderLayer(d_model=64, nhead=4, dim_feedforward=512, dropout=0.4,
+                                                  activation='relu')
         self.transf_encoder = nn.TransformerEncoder(transf_layer, num_layers=4)
         # Linear softmax layer
-        self.out_linear = nn.Linear(320,num_emotions)
+        self.out_linear = nn.Linear(320, num_emotions)
         self.dropout_linear = nn.Dropout(p=0)
         self.out_softmax = nn.Softmax(dim=1)
-    def forward(self,x):
+
+    def forward(self, x):
         # conv embedding
-        conv_embedding = self.conv2Dblock(x) #(b,channel,freq,time)
-        conv_embedding = torch.flatten(conv_embedding, start_dim=1) # do not flatten batch dimension
+        conv_embedding = self.conv2Dblock(x)  # (b,channel,freq,time)
+        conv_embedding = torch.flatten(conv_embedding, start_dim=1)  # do not flatten batch dimension
         # transformer embedding
         x_reduced = self.transf_maxpool(x)
-        x_reduced = torch.squeeze(x_reduced,1)
-        x_reduced = x_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
+        x_reduced = torch.squeeze(x_reduced, 1)
+        x_reduced = x_reduced.permute(2, 0, 1)  # requires shape = (time,batch,embedding)
         transf_out = self.transf_encoder(x_reduced)
         transf_embedding = torch.mean(transf_out, dim=0)
         # concatenate
@@ -80,5 +84,6 @@ class ParallelModel(nn.Module):
         output_softmax = self.out_softmax(output_logits)
         return output_logits, output_softmax
 
+
 def loss_fnc(predictions, targets):
-    return nn.CrossEntropyLoss()(input=predictions,target=targets)
+    return nn.CrossEntropyLoss()(input=predictions, target=targets)
